140582adde97e23ce56209f955b436082718d24f
angela64711 <angela.kiratzidou@gmail.com>
2026-03-08 09:52:04 +0100

    Create country-data.js file and add all the necessary data.

---
+++ assets/js/country-data.js
@@ -0,0 +1,140 @@
+/* Country data */
+
+const countries = [
+    {
+        name: "Austria",
+        code: "at", 
+        capital: "Vienna"
+    },
+     {
+        name: "Belgium",
+        code: "be", 
+        capital: "Brussels"
+    },
+     {
+        name: "Bulgaria",
+        code: "bg", 
+        capital: "Sofia"
+    },
+     {
+        name: "Cyprus",
+        code: "cy", 
+        capital: "Nicosia"
+    },
+     {
+        name: "Czechia",
+        code: "cz", 
+        capital: "Prague"
+    },
+     {
+        name: "Germany",
+        code: "de", 
+        capital: "Berlin"
+    },
+     {
+        name: "Denmark",
+        code: "dk", 
+        capital: "Copenhagen"
+    },
+     {
+        name: "Estonia",
+        code: "ee", 
+        capital: "Tallinn"
+    },
+     {
+        name: "Spain",
+        code: "es", 
+        capital: "Madrid"
+    },
+     {
+        name: "Finland",
+        code: "fi", 
+        capital: "Helsinki"
+    },
+     {
+        name: "France",
+        code: "fr", 
+        capital: "Paris"
+    },
+     {
+        name: "Greece",
+        code: "gr", 
+        capital: "Athens"
+    },
+     {
+        name: "Croatia",
+        code: "hr", 
+        capital: "Zagreb"
+    },
+     {
+        name: "Hungary",
+        code: "hu", 
+        capital: "Budapest"
+    },
+     {
+        name: "Ireland",
+        code: "ie", 
+        capital: "Dublin"
+    },
+     {
+        name: "Italy",
+        code: "it", 
+        capital: "Rome"
+    },
+     {
+        name: "Lithuania",
+        code: "lt", 
+        capital: "Vilnius"
+    },
+     
+     {
+        name: "Luxembourg",
+        code: "lu", 
+        capital: "Luxembourg"
+    },
+    {
+        name: "Latvia",
+        code: "lv", 
+        capital: "Riga"
+    },
+     {
+        name: "Malta",
+        code: "mt", 
+        capital: "Valletta"
+    },
+    {
+        name: "Netherlands",
+        code: "nl", 
+        capital: "Amsterdam"
+    },
+    {
+        name: "Poland",
+        code: "pl", 
+        capital: "Warsaw"
+    },
+    {
+        name: "Portugal",
+        code: "pt", 
+        capital: "Lisbon"
+    },
+    {
+        name: "Romania",
+        code: "ro", 
+        capital: "Bucharest"
+    },
+    {
+        name: "Sweden",
+        code: "se", 
+        capital: "Stockholm"
+    },
+    {
+        name: "Slovenia",
+        code: "si", 
+        capital: "Ljubljana"
+    },
+    {
+        name: "Slovakia",
+        code: "sk", 
+        capital: "Bratislava"
+    },
+]
